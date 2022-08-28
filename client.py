0ce53836647518bdde512a672ec4b9966c09a052
aashishanand89 <70255255+aashishanand89@users.noreply.github.com>
2022-08-29 09:27:41 +1000

    Create client.py

---
+++ client.py
@@ -0,0 +1,12 @@
+import paho.mqtt.client as mqtt
+def on_connect(client, userdata, flags, rc): # func for making connection
+ print("Connected to MQTT")
+ print("Connection returned result: " + str(rc) )
+ client.subscribe("ifn649")
+def on_message(client, userdata, msg): # Func for Sending msg
+ print(msg.topic+" "+str(msg.payload))
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("18.142.96.176", 1883, 60)
+client.loop_forever()
